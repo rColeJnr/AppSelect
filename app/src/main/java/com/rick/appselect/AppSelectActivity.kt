dadbe4a9fc3832c9e56f62ff8b4785a4a4bc92c2
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-02 22:34:52 +0300

    initial commit

---
+++ app/src/main/java/com/rick/appselect/AppSelectActivity.kt
@@ -0,0 +1,17 @@
+package com.rick.appselect
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rick.appselect.databinding.ActivityAppSelectBinding
+
+class AppSelectActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAppSelectBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAppSelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
